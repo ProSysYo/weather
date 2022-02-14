ae8d14097388eabb0d9e1795b0712f36614694ba
Maxim Ivanov <maksimivanov91@yandex.ru>
2022-02-14 10:38:44 +0300

    .

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,7 @@ const getWetherByCityName = async (city: string) => {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_KEY}&lang=ru`);
     
     if (response.ok) {
-        let json = await response.json();
-        console.log(json);
+        let json = await response.json();        
         
         return json;
     } else {
